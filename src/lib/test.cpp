888cfd9651fe89e62f07bb59b66b2478f06538b6
xianliang <xianliang.li@zilliz.com>
2022-08-31 13:25:35 +0800

    fix test

--- src/lib/test.cpp
+++ src/lib/test.cpp
@@ -1,7 +1,16 @@
 #include "lib/bbannlib2.h"
 
-int main() {
+int main(int argc, char **argv) {
+/*  std::cout << " raw_data_bin_file: " << dataFilePath_
+            << " output_path: " << indexPrefix_ << " hnsw.M: " << para.hnswM
+            << " hnsw.efConstruction: " << para.hnswefC << " K1: " << para.K1
+            << std::endl;*/
   bbann::BBAnnParameters para;
+  para.dataFilePath = argv[1];
+  para.indexPrefixPath =  para.dataFilePath + "index_";
+  para.hnswM = 16;
+  para.hnswefC = 200;
+  para.efSearch = 200;
   auto index =
       std::make_unique<bbann::BBAnnIndex2<float, float>>(MetricType::L2);
   bbann::BBAnnIndex2<float, float>::BuildIndex(para);
